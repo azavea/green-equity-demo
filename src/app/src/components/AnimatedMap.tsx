7c7f52897a4302d9b7951950a0aa44a744a26049
Rachele Morino <rmorino@azavea.com>
2023-02-24 18:44:01 -0500

    Fix setting state in useEffect

--- src/app/src/components/AnimatedMap.tsx
+++ src/app/src/components/AnimatedMap.tsx
@@ -34,27 +34,28 @@ function StatesAndSliderLayer({
     spending: MonthlySpendingOverTimeResponse;
 }) {
     const SLIDER_PRESENT_STEP = 26;
-    const [timeValue, setTimeValue] = useState(0);
     const map = useMap();
     const [spendingAtTimeByState, setSpendingAtTimeByState] = useState(() => getspendingByStateAtTime(1, spending));
 
     useEffect(()=>{
-        setSpendingAtTimeByState(getspendingByStateAtTime(timeValue, spending));
         // @ts-ignore
-        map && !!timeValue && map.eachLayer((l) => l.feature && l.setStyle({fillcolor: getColor(spendingAtTimeByState[l.feature.properties.STUSPS]?.aggregated_amount)}));
-    }, [map, timeValue])
+        map && map.eachLayer((l) => l.feature && l.setStyle({fillcolor: getColor(spendingAtTimeByState[l.feature.properties.STUSPS]?.aggregated_amount)}));
+    }, [map, spendingAtTimeByState])
+
+    function onSliderChange(timeValue: number){
+        setSpendingAtTimeByState(getspendingByStateAtTime(timeValue, spending));
+    }
 
     return (
         <>
             <StatesLayer
                 onEachFeature={(feature, layer) => {
                     const defaultFillColor = getColor(spendingAtTimeByState[feature.properties.STUSPS]?.aggregated_amount)
-                    // maybe need to update geojson types?
                     // @ts-ignore
                     layer && layer.setStyle({fill: true, fillColor: defaultFillColor, fillOpacity: 100 });
                 }}
             />
-            <Slider colorScheme={'blackAlpha'} aria-label='date-time-slider' defaultValue={0} min={0} max={SLIDER_PRESENT_STEP} onChange={(val) => setTimeValue(val)} step={1} mt='575px' width='50%' ml='20%'>
+            <Slider colorScheme={'blackAlpha'} aria-label='date-time-slider' defaultValue={0} min={0} max={SLIDER_PRESENT_STEP} onChange={(val) => onSliderChange(val)} step={1} mt='575px' width='50%' ml='20%'>
                 <SliderThumb ml='50px'/>
                 <SliderMark value={0} mt='-2' mr='15' fontSize='m'>
                         2021
