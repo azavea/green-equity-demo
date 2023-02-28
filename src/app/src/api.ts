6e267628432e038b9866dbe007e08f63ebc1ff5b
Matt Stone <mstone@element84.com>
2023-02-28 12:54:03 -0600

    Swap RTK Query query fn with cached version

--- src/app/src/api.ts
+++ src/app/src/api.ts
@@ -1,9 +1,15 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query/react';
 import {
     SpendingByGeographyRequest,
     SpendingByGeographyResponse,
     State,
 } from './types/api';
+
+/* Uncomment this to use the api */
+/* import {  fetchBaseQuery } from '@reduxjs/toolkit/query/react'; */
+
+/* Uncomment this to use cached data */
+import fetchBaseQuery from './cachedApiQuery';
 
 export const spendingApiUrl = 'https://api.usaspending.gov/api/v2';
 
