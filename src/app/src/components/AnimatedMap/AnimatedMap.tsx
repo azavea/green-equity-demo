1a6a0e1d01921045d699062eb372a7dad51c57bf
Rachele Morino <rmorino@azavea.com>
2023-03-08 18:31:52 -0500

    Add initial arc path per state

--- src/app/src/components/AnimatedMap/AnimatedMap.tsx
+++ src/app/src/components/AnimatedMap/AnimatedMap.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useMap } from 'react-leaflet';
 import {
     Center,
@@ -15,6 +15,7 @@ import {
 import L from 'leaflet';
 import UsaMapContainer from '../UsaMapContainer';
 import { StateGeometry, StateProperties } from '../states.geojson';
+import '@elfalem/leaflet-curve';
 
 import StatesLayer from '../StatesLayer';
 import TimeControlIcon from './TimeControlIcon';
@@ -26,6 +27,8 @@ import {
 import AnimatedMapLegend from './AnimatedMapLegend';
 import { useGetSpendingOverTimeQuery } from '../../api';
 
+const DC_CENTER: L.LatLngTuple = [1.6467356667879738, 14.997499934940763];
+
 const START_YEAR = 2021;
 const END_DATE = new Date();
 const PROGRESS_FINAL_STEP = (() => {
@@ -33,6 +36,26 @@ const PROGRESS_FINAL_STEP = (() => {
     const final_year_step = END_DATE.getFullYear();
     return 12 * (final_year_step - START_YEAR) + final_month_step;
 })();
+
+function getBezierOffsetLatLng(end: L.LatLngTuple): L.LatLngTuple {
+    // Modified Bezier curve function from https://gist.github.com/ryancatalani/6091e50bf756088bf9bf5de2017b32e6
+    const start: L.LatLngTuple = DC_CENTER;
+    var offsetX = end[1] - start[1],
+        offsetY = end[0] - start[0];
+
+    var r = Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2)),
+        theta = Math.atan2(offsetY, offsetX);
+
+    var thetaOffset = 3.14 / 10;
+
+    var r2 = r / 2 / Math.cos(thetaOffset),
+        theta2 = theta + thetaOffset;
+
+    var midpointX = r2 * Math.cos(theta2) + start[1],
+        midpointY = r2 * Math.sin(theta2) + start[0];
+
+    return [midpointY, midpointX];
+}
 
 export default function AnimatedMap() {
     const { data, isFetching } = useGetSpendingOverTimeQuery();
@@ -137,12 +160,16 @@ function StatesAndSliderLayer({
     timeValue: number;
 }) {
     const map = useMap();
-
+    const arcPathsReference = useRef<L.Curve[]>([]);
     const [spendingAtTimeByState, setSpendingAtTimeByState] = useState(() =>
         getSpendingByStateAtTime(1, spending)
     );
 
     useEffect(() => {
+        map &&
+            arcPathsReference.current.forEach(path => {
+                path.addTo(map);
+            });
         map &&
             map.eachLayer(l => {
                 const asGeoJson = l as L.GeoJSON<
@@ -180,6 +207,32 @@ function StatesAndSliderLayer({
                     feature,
                     layer: L.GeoJSON<StateProperties, StateGeometry>
                 ) => {
+                    layer.on('add', event => {
+                        const polygonCenter = (event.sourceTarget as L.Polygon)
+                            .getBounds()
+                            .getCenter();
+                        const polygonCenterTuple: L.LatLngTuple = [
+                            polygonCenter.lat,
+                            polygonCenter.lng,
+                        ];
+                        const midpoint: L.LatLngTuple =
+                            getBezierOffsetLatLng(polygonCenterTuple);
+
+                        arcPathsReference.current.push(
+                            new L.Curve(
+                                [
+                                    'M',
+                                    DC_CENTER,
+                                    'Q',
+                                    midpoint,
+                                    polygonCenterTuple,
+                                ],
+                                {
+                                    color: '#2051FF',
+                                }
+                            )
+                        );
+                    });
                     const defaultFillColor = getColor(
                         spendingAtTimeByState[
                             feature.properties.STUSPS.toString()
